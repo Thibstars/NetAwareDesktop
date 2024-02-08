9539b67aebc9132d1ebdf1dcb7c4c69cf0a37ce6
Thibstars <thibault.helsmoortel@gmail.com>
2024-02-08 16:51:25 +0100

    Initial commit

---
+++ src/main/java/com/github/thibstars/netaware/desktop/MainFrame.java
@@ -0,0 +1,59 @@
+package com.github.thibstars.netaware.desktop;
+
+import com.github.thibstars.netaware.events.IpAddressFoundEvent;
+import com.github.thibstars.netaware.events.IpScannerEvent;
+import com.github.thibstars.netaware.events.core.EventManager;
+import com.github.thibstars.netaware.scanners.IpScanner;
+import com.github.thibstars.netaware.scanners.IpScannerInput;
+import java.awt.Dimension;
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ * @author Thibault Helsmoortel
+ */
+public class MainFrame extends JFrame {
+
+    private final JTable resultTable;
+    private final DefaultTableModel tableModel;
+
+    public MainFrame() throws HeadlessException {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        Dimension dimension = new Dimension(750, 500);
+        setSize(dimension);
+        setLocationRelativeTo(null);
+
+        tableModel = new DefaultTableModel();
+        tableModel.addColumn("IP Address");
+        resultTable = new JTable(tableModel);
+        JScrollPane scrollPane = new JScrollPane(resultTable);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        JPanel mainPanel = new JPanel();
+        mainPanel.setSize(dimension);
+        mainPanel.add(scrollPane);
+        add(mainPanel);
+
+        discoverDevices();
+
+        setVisible(true);
+    }
+
+    private void discoverDevices() {
+        EventManager eventManager = new EventManager();
+
+        eventManager.registerHandler(
+                IpAddressFoundEvent.class,
+                event -> {
+                    // Add result to table
+                    tableModel.addRow(new Object[]{event.getIpAddress().getHostAddress()});
+                });
+
+        IpScanner ipScanner = new IpScanner(eventManager);
+        ipScanner.scan(new IpScannerInput("192.168.1.0", 254));
+    }
+}
---
+++ src/main/java/com/github/thibstars/netaware/desktop/NetawareDesktop.java
@@ -0,0 +1,12 @@
+package com.github.thibstars.netaware.desktop;
+
+/**
+ * @author Thibault Helsmoortel
+ */
+public class NetawareDesktop {
+
+    public static void main(String[] args) {
+        new MainFrame();
+    }
+
+}
