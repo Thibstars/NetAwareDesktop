ec48504f7e85e86ea7e57650cd6f8cdfbe87989c
Thibstars <thibault.helsmoortel@gmail.com>
2024-02-09 17:17:45 +0100

    Scan for ips, ports and mac

--- src/main/java/com/github/thibstars/netaware/desktop/MainFrame.java
+++ src/main/java/com/github/thibstars/netaware/desktop/MainFrame.java
@@ -1,13 +1,21 @@
 package com.github.thibstars.netaware.desktop;
 
 import com.github.thibstars.netaware.events.IpAddressFoundEvent;
+import com.github.thibstars.netaware.events.MacFoundEvent;
+import com.github.thibstars.netaware.events.TcpIpPortFoundEvent;
 import com.github.thibstars.netaware.events.core.EventManager;
 import com.github.thibstars.netaware.scanners.IpScanner;
 import com.github.thibstars.netaware.scanners.IpScannerInput;
+import com.github.thibstars.netaware.scanners.MacScanner;
+import com.github.thibstars.netaware.scanners.PortScanner;
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.HeadlessException;
+import java.net.InetAddress;
+import java.util.stream.IntStream;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ScrollPaneConstants;
@@ -19,6 +27,7 @@ import javax.swing.table.DefaultTableModel;
 public class MainFrame extends JFrame {
 
     private final DefaultTableModel tableModel;
+    private final JProgressBar iPProgressBar;
 
     public MainFrame() throws HeadlessException {
         setTitle("NetAware Desktop");
@@ -29,31 +38,78 @@ public class MainFrame extends JFrame {
 
         tableModel = new DefaultTableModel();
         tableModel.addColumn("IP Address");
+        tableModel.addColumn("Open Ports");
+        tableModel.addColumn("MAC Address");
         JTable resultTable = new JTable(tableModel);
         JScrollPane scrollPane = new JScrollPane(resultTable);
         scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         JPanel mainPanel = new JPanel();
         mainPanel.setSize(dimension);
-        mainPanel.add(scrollPane);
+        mainPanel.setLayout(new BorderLayout());
+        iPProgressBar = new JProgressBar();
+        iPProgressBar.setToolTipText("Scan in progress...");
+        iPProgressBar.setIndeterminate(true);
+        iPProgressBar.setVisible(false);
+        JPanel pnlProgressBars = new JPanel();
+        pnlProgressBars.add(iPProgressBar);
+        mainPanel.add(pnlProgressBars, BorderLayout.PAGE_START);
+        mainPanel.add(scrollPane, BorderLayout.PAGE_END);
         add(mainPanel);
 
-        discoverDevices();
-
         setVisible(true);
+
+        discoverDevices();
     }
 
     private void discoverDevices() {
         EventManager eventManager = new EventManager();
 
+        eventManager.registerHandler(TcpIpPortFoundEvent.class, event -> {
+            InetAddress ipAddress = event.getIpAddress();
+            Integer port = event.getTcpIpPort();
+
+            IntStream.range(0, tableModel.getRowCount())
+                    .filter(index -> tableModel.getValueAt(index, 0).equals(ipAddress.getHostAddress()))
+                    .findFirst()
+                    .ifPresent(index -> {
+                        Object valueAt = tableModel.getValueAt(index, 1);
+                        String newValue = valueAt != null && !((String) valueAt).isBlank() ? valueAt + ", " + port : String.valueOf(port);
+                        tableModel.setValueAt(newValue, index, 1);
+                    });
+        });
+
+        eventManager.registerHandler(MacFoundEvent.class, event -> {
+            InetAddress ipAddress = event.getIpAddress();
+            String macAddress = event.getMacAddress();
+
+            IntStream.range(0, tableModel.getRowCount())
+                    .filter(index -> tableModel.getValueAt(index, 0).equals(ipAddress.getHostAddress()))
+                    .findFirst()
+                    .ifPresent(index -> tableModel.setValueAt(macAddress, index, 2));
+        });
+
+        IpScanner ipScanner = new IpScanner(eventManager);
+        PortScanner portScanner = new PortScanner(eventManager);
+        MacScanner macScanner = new MacScanner(eventManager);
+
+        String firstIpInTheNetwork = "192.168.1.0";
+        int amountOfIpsToScan = 254;
+
         eventManager.registerHandler(
                 IpAddressFoundEvent.class,
                 event -> {
-                    // Add result to table
-                    tableModel.addRow(new Object[]{event.getIpAddress().getHostAddress()});
+                    if (event instanceof IpAddressFoundEvent ipAddressFoundEvent) {
+                        InetAddress ipAddress = ipAddressFoundEvent.getIpAddress();
+                        String hostAddress = ipAddress.getHostAddress();
+                        tableModel.addRow(new Object[]{hostAddress, "", ""});
+                        portScanner.scan(ipAddress);
+                        macScanner.scan(ipAddress);
+                    }
                 });
 
-        IpScanner ipScanner = new IpScanner(eventManager);
-        ipScanner.scan(new IpScannerInput("192.168.1.0", 254));
+        iPProgressBar.setVisible(true);
+        ipScanner.scan(new IpScannerInput(firstIpInTheNetwork, amountOfIpsToScan));
+        iPProgressBar.setVisible(false);
     }
 }
