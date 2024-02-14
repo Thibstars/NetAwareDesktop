43d89205178760fa998d796dc7f0d853547eeeac
Thibstars <thibault.helsmoortel@gmail.com>
2024-02-14 16:51:14 +0100

    Fix casing in main class

--- src/main/java/com/github/thibstars/netaware/desktop/NetAwareDesktop.java
+++ src/main/java/com/github/thibstars/netaware/desktop/NetAwareDesktop.java
@@ -3,7 +3,7 @@ package com.github.thibstars.netaware.desktop;
 /**
  * @author Thibault Helsmoortel
  */
-public class NetawareDesktop {
+public class NetAwareDesktop {
 
     public static void main(String[] args) {
         new MainFrame();
